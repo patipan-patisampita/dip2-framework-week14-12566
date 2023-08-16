64a745b9ca0914e0caed3a099cf6f06bbf14a5e2
patipan-patisampita <patipan-patisampita@gmail.com>
2023-08-16 15:33:13 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -362,11 +362,11 @@
     <!-- REQUIRED SCRIPTS -->
 
     <!-- jQuery -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+    <script src="{{ asset('lte/plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
-    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('lte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="dist/js/adminlte.min.js"></script>
+    <script src="{{ asset('lte/js/adminlte.min.js') }}"></script>
 </body>
 
 </html>
